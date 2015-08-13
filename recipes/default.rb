3c53cbc0e537d426acd6e164fb8dcb4d91fb9d3c
Andrew Jones <andrew@jones.ec>
2015-08-13 00:03:50 -0400

    Don't configure nvidia on non-nvidia systems.

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,7 +13,18 @@ include_recipe 'desktop::apt'
 # Hardware.
 include_recipe 'desktop::pc-speaker'
 include_recipe 'desktop::bluetooth'
-include_recipe 'desktop::nvidia'
+
+package 'pciutils'
+
+# Don't configure nvidia drivers on non-nvidia systems.
+if system('lspci | grep VGA | grep -i nvidia') 
+  include_recipe 'desktop::nvidia'
+else
+  log 'This system does not contain an nVidia GPU'
+  file '/etc/X11/xorg.conf.d/20-nvidia.conf' do
+    action :delete
+  end
+end
 
 # Software.
 include_recipe 'desktop::applications'
