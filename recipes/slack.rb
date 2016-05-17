62a160a44b34497d5bfeb606b16a255d162e3cfe
Andrew Jones <andrew@jones.ec>
2016-05-17 13:56:24 -0400

    Update for non-beta slack releases

--- recipes/slack.rb
+++ recipes/slack.rb
@@ -22,16 +22,18 @@ package [
          'python', 
          'gvfs-bin', 
          'xdg-utils',
-         'apt-transport-https'
+         'apt-transport-https',
+         'libgnome-keyring0',
         ] do
   action :install
 end
 
-slack_deb_path = "#{Chef::Config[:file_cache_path]}/slack-desktop-amd64.deb"
-
-remote_file slack_deb_path do
-  source 'https://slack-ssb-updates.global.ssl.fastly.net/linux_releases/slack-desktop-1.2.5-amd64.deb'
-  checksum '2368e2449f08272d8ce38a423f44ed0cd35c1e77ca3c0525f069d0790b6ec58f'
+# Slack doesn't have official Ubuntu/Trusty repos, so use Jessie everywhere.
+apt_repository 'slacktechnologies_slack' do
+  components ['main']
+  distribution 'jessie'
+  key 'https://packagecloud.io/slacktechnologies/slack/gpgkey'
+  uri "https://packagecloud.io/slacktechnologies/slack/debian/"
 end
 
-dpkg_package slack_deb_path
+package 'slack-desktop'
