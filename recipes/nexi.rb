2ae25a5c4c2d0667d26331749dc9f6f26152d22c
Andrew Jones <andrew@jones.ec>
2016-05-18 18:07:33 -0400

    Update Vidyo client to newest package

--- recipes/nexi.rb
+++ recipes/nexi.rb
@@ -1,10 +1,10 @@
 
 
-deb_name = 'VidyoDesktopInstaller-ubuntu64-TAG_VD_3_5_4_010.deb'
+deb_name = 'VidyoDesktopInstaller-ubuntu64-TAG_VD_3_6_3_017.deb'
 deb_path = "#{Chef::Config[:file_cache_path]}/#{deb_name}"
 remote_file deb_path do
   source "https://demo.vidyo.com/upload/#{deb_name}"
-  checksum '9c5ab8293b6888336bc8d7a6d8b6d097f40efd201966c18cfd983f8adf58627c'
+  checksum '9d2455dc29bfa7db5cf3ec535ffd2a8c86c5a71f78d7d89c40dbd744b2c15707'
 end
 
 package 'libqt4-gui'
