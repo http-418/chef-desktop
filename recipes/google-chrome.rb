b89f0caa9106f9896361641cf0a267b44d1469c7
Andrew Jones <andrew@jones.ec>
2016-06-22 16:04:37 -0400

    Update google chrome repo

--- recipes/google-chrome.rb
+++ recipes/google-chrome.rb
@@ -8,11 +8,17 @@
 #
 
 apt_repository 'google-chrome-unstable' do
-  uri 'http://dl.google.com/linux/chrome/deb/'
+  uri 'http://dl.google.com/linux/deb/'
   arch 'amd64'
   components ['stable', 'main']
   keyserver 'keyserver.ubuntu.com'
   key 'A040830F7FAC5991'
+end
+
+if Gem::Version.new(node[:platform_version]) >= Gem::Version.new('16.04')
+  log 'Forcing apt update to work around apt cookbook bugs on 16.04' do
+    notifies :run, 'execute[apt-get update]', :immediately
+  end
 end
 
 package [ 'google-chrome-unstable', 'google-chrome-beta' ] do
