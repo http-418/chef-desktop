31e08b2419e225972583d715d01dab6f72fbd4a7
Andrew Jones <andrew@jones.ec>
2016-10-10 10:20:15 -0400

    Update vidyo recipe for 16.04

--- recipes/vidyo.rb
+++ recipes/vidyo.rb
@@ -22,20 +22,88 @@
 # particularly useful unless you happen to have the proprietary Vidyo
 # server installed somewhere.
 #
-deb_name = 'VidyoDesktopInstaller-ubuntu64-TAG_VD_3_6_3_017.deb'
-deb_path = "#{Chef::Config[:file_cache_path]}/#{deb_name}"
-remote_file deb_path do
-  source "https://demo.vidyo.com/upload/#{deb_name}"
-  checksum '9d2455dc29bfa7db5cf3ec535ffd2a8c86c5a71f78d7d89c40dbd744b2c15707'
-end
+
+include_recipe 'desktop::apt'
 
 package [
-          'libqt4-designer',
-          'libqt4-opengl',
-          'libqt4-svg',
-          'libqtgui4'
+          'libxss1',
+          'libasound2'
         ] do
   action :upgrade
 end
 
-dpkg_package deb_path
+debian_before_stretch =
+  node[:platform] == 'debian' &&
+  Gem::Version.new(node[:lsb][:release]) < Gem::Version.new('9.0')
+
+ubuntu_before_xenial =
+  node[:platform] == 'ubuntu' &&
+  Gem::Version.new(node[:lsb][:release]) < Gem::Version.new('16.04')
+
+if (debian_before_stretch || ubuntu_before_xenial)
+  package 'libqt4-gui' do
+    action :upgrade
+  end
+else
+  package [
+            'libqt4-designer',
+            'libqt4-opengl',
+            'libqt4-svg',
+            'libqtgui4',
+            'equivs'
+          ] do
+    action :upgrade
+  end
+
+  build_directory =
+    File.join(Chef::Config.file_cache_path, 'libqt4-gui')
+
+  directory build_directory do
+    action :create
+    mode 0o755
+  end
+
+  control_file_path =
+    File.join(build_directory, 'control_file')
+
+  package_path =
+    File.join(build_directory,'libqt4-gui_4.8.1_amd64.deb')
+
+  file control_file_path do
+    mode 0o444
+    content <<-EOM.gsub(/^ {6}/,'')
+      Section: misc
+      Priority: optional
+      Standards-Version: 3.9.20
+
+      Package: libqt4-gui
+      Version: 4:4.8.1
+      Maintainer: Andrew Jones <andrew@jones.ec>
+      Architecture: amd64
+      Description: Dummy package to satisfy legacy dependency.
+      Depends: libqt4-designer (>= 4), libqt4-opengl (>= 4), \
+               libqt4-svg (>= 4), libqtgui4 (>= 4)
+    EOM
+  end
+
+  execute 'libqt4-gui-build' do
+    require 'shellwords'
+    cwd build_directory
+    command "equivs-build -a amd64 #{control_file_path.shellescape}"
+    creates package_path
+  end
+
+  dpkg_package 'libqt4-gui' do
+    source package_path
+  end
+end
+
+vidyo_deb_name = 'VidyoDesktopInstaller-ubuntu64-TAG_VD_3_6_3_017.deb'
+vidyo_deb_path = "#{Chef::Config[:file_cache_path]}/#{vidyo_deb_name}"
+
+remote_file vidyo_deb_path do
+  source "https://demo.vidyo.com/upload/#{vidyo_deb_name}"
+  checksum '9d2455dc29bfa7db5cf3ec535ffd2a8c86c5a71f78d7d89c40dbd744b2c15707'
+end
+
+dpkg_package vidyo_deb_path
