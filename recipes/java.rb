fc8918ede4d9022d9e8cf9249dba9c3b09997a08
James Conant <james@conant.io>
2017-01-14 01:17:53 -0500

    Includes `backports` recipe dependency within `java` recipe.

--- recipes/java.rb
+++ recipes/java.rb
@@ -21,6 +21,8 @@ node.default[:java][:install_flavor] = 'oracle'
 node.default[:java][:jdk_version] = '8'
 node.default[:java][:oracle][:accept_oracle_download_terms] = true
 
+include_recipe 'desktop::backports'
+
 # Install a packaged java to satisfy Debian deps.
 package 'openjdk-8-jdk'
 
