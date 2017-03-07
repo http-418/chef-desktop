a3246dea639f50ef834678f930b91555e73a9512
James Conant <james@conant.io>
2017-03-07 07:39:15 -0500

    Update Ruby recipe to be more configurable.

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -30,26 +30,28 @@ node.default[:rbenv][:gems] = node[:rbenv][:rubies]
 include_recipe 'ruby_build'
 include_recipe 'ruby_rbenv::system'
 
-rbenv_global '2.3.0'
+rbenv_global node[:rbenv].fetch(:rbenv_global, '2.3.0')
 
-ruby_block 'uninstall-old-rubies' do
-  block do
-    require 'mixlib/shellout'
-    rbenv = node[:rbenv][:root_path] + '/bin/rbenv'
-    
-    list_command = Mixlib::ShellOut.new(rbenv,'versions', '--bare')
-    list_command.run_command
-    
-    raise 'Failed to list ruby versions!' unless list_command.status.success?
-    
-    ruby_versions = list_command.stdout.split("\n")
-    versions_to_remove =
-      ruby_versions.reject{ |v| node[:rbenv][:rubies].include?(v) }
+unless node[:rbenv].fetch(:keep_old_rubies, false)
+  ruby_block 'uninstall-old-rubies' do
+    block do
+      require 'mixlib/shellout'
+      rbenv = node[:rbenv][:root_path] + '/bin/rbenv'
 
-    versions_to_remove.each do |v|
-      remove_command = Mixlib::ShellOut.new(rbenv, 'uninstall', '-f', v)
-      remove_command.run_command
-      raise "Failed to remove ruby #{v}!" unless remove_command.status.success?
+      list_command = Mixlib::ShellOut.new(rbenv,'versions', '--bare')
+      list_command.run_command
+
+      raise 'Failed to list ruby versions!' unless list_command.status.success?
+
+      ruby_versions = list_command.stdout.split("\n")
+      versions_to_remove =
+          ruby_versions.reject{ |v| node[:rbenv][:rubies].include?(v) }
+
+      versions_to_remove.each do |v|
+        remove_command = Mixlib::ShellOut.new(rbenv, 'uninstall', '-f', v)
+        remove_command.run_command
+        raise "Failed to remove ruby #{v}!" unless remove_command.status.success?
+      end
     end
   end
 end
