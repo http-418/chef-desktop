0775ca034471e28b95575959b8bb8a90047bfeb0
Andrew Jones <andrew@jones.ec>
2017-05-04 15:43:57 -0400

    Add hidden dep for eod

--- recipes/eod.rb
+++ recipes/eod.rb
@@ -17,15 +17,19 @@
 # limitations under the License.
 #
 
+package 'libpangox-1.0-0' do
+  action :upgrade
+end
+
 #
 # This recipe installs the Exceed On Demand client for Linux.
 # It does not configure the corresponding MIME handler.
 #
-eod_tarball_path = 
+eod_tarball_path =
   Chef::Config['file_cache_path'] + '/eodclient8-13.8.3-linux-x64.tar.gz'
 
-remote_file eod_tarball_path do 
-  source 'http://mimage.opentext.com/patches/connectivity/eod8/_ed0wn15/e0dc1n7/linux/eodclient8-13.8.3-linux-x64.tar.gz' 
+remote_file eod_tarball_path do
+  source 'http://mimage.opentext.com/patches/connectivity/eod8/_ed0wn15/e0dc1n7/linux/eodclient8-13.8.3-linux-x64.tar.gz'
   checksum '9834a630863fbbcbeffbbeff4441eee0167fd2d1f71b36bccdcfec5c083ec139'
 end
 
