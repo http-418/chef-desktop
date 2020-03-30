c1faf1ed3970e97fe48ffe8d1ef316478868d98e
Andrew Jones <andrew@jones.ec>
2020-03-30 19:06:04 -0400

    Switch from sddm to gdm3

--- attributes/display_manager.rb
+++ attributes/display_manager.rb
@@ -1,10 +1,2 @@
-# Ubuntu 16.04 and above lack a KDM package.
-dm = if node[:platform] == 'ubuntu' &&
-         Gem::Version.new(node[:platform_version]) >= Gem::Version.new('16.04')
-       'sddm'
-     else
-       'kdm'
-     end
-
-default['desktop']['display_manager'] = dm
-
+# This used to be platform dependent, not anymore.
+default['desktop']['display_manager'] = 'gdm3'
