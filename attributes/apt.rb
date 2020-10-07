4270a3ba1a329b12b986462b5f6fb76b67498084
Andrew Jones <andrew@jones.ec>
2020-10-07 16:40:32 -0400

    Add multiverse :(

--- attributes/apt.rb
+++ attributes/apt.rb
@@ -2,3 +2,4 @@ default['debian']['mirror'] =
   'http://mirror.cc.columbia.edu/debian'
 default['ubuntu']['mirror'] =
   'http://mirror.cc.columbia.edu/pub/linux/ubuntu/archive'
+default['ubuntu']['multiverse_enabled'] = false
